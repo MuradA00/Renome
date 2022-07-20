4c1c69c6b0338742119a8471e61c7c2904522b1c
Murad <abishlimurad@gmail.com>
2022-07-20 05:01:08 +0200

    services inner page

--- source/js/common.js
+++ source/js/common.js
@@ -46,11 +46,12 @@
 
 const newsRow = new Swiper('.journal__inner', {
   slidesPerView: 1,
-  spaceBetween: 10,
   speed: 500,
+  noSwiping: true,
   breakpoints: {
     800: {
-      slidesPerView: 3
+      spaceBetween: 34.5,
+      slidesPerView: 'auto'
     }
   },
   pagination: {
@@ -61,6 +62,26 @@ const newsRow = new Swiper('.journal__inner', {
     prevEl: '.journal__btn-prev',
   },
 });
+
+const stockRow = new Swiper('.stock__inner', {
+  slidesPerView: 1,
+  speed: 500,
+  noSwiping: true,
+  breakpoints: {
+    800: {
+      spaceBetween: 34.5,
+      slidesPerView: 'auto'
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.stock__btn-next',
+    prevEl: '.stock__btn-prev',
+  },
+});
+
 
 const diplomaRow = new Swiper('.achiev__slider-wrapper', {
   slidesPerView: 1,
@@ -94,20 +115,54 @@ const employeesaRow = new Swiper('.empl__list', {
   },
 });
 
-// const clientRow = new Swiper('.client__list', {
-//   slidesPerView: 5,
-//   speed: 500,
-//   breakpoints: {
-//     900: {
-//       slidesPerView: 5
-//     }
-//   },
-//   // loop: true,
-//   navigation: {
-//     nextEl: '.client__btn-next',
-//     prevEl: '.client__btn-prev',
-//   },
-// });
+const clientRow = new Swiper('.client__list', {
+  slidesPerView: 'auto',
+  speed: 600,
+  grabCursor: true,
+  paginationClickable: true,
+  mousewheelReleaseOnEdges: true,
+  // loop: true,
+  navigation: {
+    nextEl: '.client__btn-next',
+    prevEl: '.client__btn-prev',
+  },
+});
+
+const clientText = new Swiper('.review__wrapper', {
+  slidesPerView: 'auto',
+  speed: 300,
+  draggable: false,
+  preventInteractionOnTransition: false,
+  allowTouchMove: false,
+  // loop: true,
+  navigation: {
+    nextEl: '.pag-next',
+    prevEl: '.pag-prev',
+  },
+});
+
+const clientName = new Swiper('.review__inner', {
+  slidesPerView: 'auto',
+  speed: 200,
+  preventInteractionOnTransition: false,
+  allowTouchMove: false,
+  // loop: true,
+  navigation: {
+    nextEl: '.pag-next',
+    prevEl: '.pag-prev',
+  },
+});
+
+const structureRow = new Swiper('.structure__inner', {
+  slidesPerView: 1,
+  setWrapperSize: true,
+  speed: 300,
+  // loop: true,
+  navigation: {
+    nextEl: '.structure__btn-next',
+    prevEl: '.structure__btn-prev',
+  },
+});
 
 if (menuLinks.length > 0) {
   menuLinks.forEach(function (menuLink) {
